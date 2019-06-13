c0912515018c43d718142b4b8607b173a2690223
Pavlin Poličar <pavlin.g.p@gmail.com>
2019-06-13 13:48:39 +0200

    Add download script for data

---
+++ data/download.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+wget file.biolab.si/tsne-embedding/baron_2016h.h5ad
+wget file.biolab.si/tsne-embedding/xin_2016.h5ad
+wget file.biolab.si/tsne-embedding/hrvatin_2018.h5ad
+wget file.biolab.si/tsne-embedding/chen_2017.h5ad
+wget file.biolab.si/tsne-embedding/macosko_2015.h5ad
+wget file.biolab.si/tsne-embedding/shekhar_2016.h5ad
